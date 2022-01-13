f384d1a2d0c6b904d7b0f10ffc1117112efd6355
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-01-13 16:38:52 +0100

    implement simple component step

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Task } from 'src/app/models/task';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+})
+export class TaskComponent {
+  @Input() task: Task;
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onPinTask = new EventEmitter<Event>();
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+
+  onPin(id: any) {
+    this.onPinTask.emit(id);
+  }
+
+  onArchive(id: any) {
+    this.onArchiveTask.emit(id);
+  }
+}
