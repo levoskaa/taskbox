b7b2a61ab135000bce7b36974c65f7a8927becfc
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-02-07 12:08:25 +0100

    implement screens step

---
+++ src/app/components/inbox-screen/inbox-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { TasksState } from 'src/app/state/task.state';
+
+@Component({
+  selector: 'app-inbox-screen',
+  templateUrl: './inbox-screen.component.html',
+})
+export class InboxScreenComponent {
+  @Select(TasksState.getError) error$: Observable<any>;
+}
